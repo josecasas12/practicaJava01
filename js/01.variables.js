3c0b4591ce89f83af225c6597f5941b589a4bd3d
jose <josecasas.trade@gmail.com>
2025-08-26 10:35:07 -0400

    agrege variables a js

--- js/01.variables.js
+++ js/01.variables.js
@@ -13,3 +13,14 @@ document.writeln("<h1 class='text-blue'>hola mundo 6</h1>");
 
 //ventanas emergentes
 //alert('practica de alert')
+
+//variables var - let - const
+
+const taiga =
+  "https://tree.taiga.io/project/earias08-comision-web16/us/74?kanban-status=10073551";
+  console.log(taiga)
+
+  document.writeln('<p>'+taiga+'</p>')
+  document.writeln(taiga)
+  document.writeln('un poco de texto')
+  
