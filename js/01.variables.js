586be2d2a98458cdf9b1605427ca60f7ce73eb94
jose <josecasas.trade@gmail.com>
2025-09-01 11:55:26 -0500

    agrege una nueva linea nextjs

--- js/01.variables.js
+++ js/01.variables.js
@@ -18,12 +18,14 @@ document.writeln("<h1 class='text-blue'>hola mundo 6</h1>");
 
 const taiga =
   "https://tree.taiga.io/project/earias08-comision-web16/us/74?kanban-status=10073551";
-  console.log(taiga)
+console.log(taiga);
 
-  document.writeln('<p>'+taiga+'</p>')
-  document.writeln(taiga)
-  document.writeln('un poco de texto')
-  
-  let curso = 'fullstack MERN'
+document.writeln("<p>" + taiga + "</p>");
+document.writeln(taiga);
+document.writeln("un poco de texto");
 
-  document.writeln('<br>'+curso)
+let curso = "fullstack MERN";
+
+document.writeln("<br>" + curso);
+curso = 'nextjs'
+document.writeln("<br>" + curso);
